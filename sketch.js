0ffbc6bcd21cb0efad3a8652be293f94e22131bd
jaihsaldhingra <73591755+jaihsaldhingra@users.noreply.github.com>
2021-01-10 16:39:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,118 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var stand1, ground1;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10
+var polygon1, slingShot;
+var score = 0;
+var backgroundImg;
+var gameState = "on slingshot";
+var bg = "Sprites/bg.jpg";
+
+function preload() {
+  getBackgroundImg();
+  backgroundImg = loadImage("Sprites/bg.jpg");
+}
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  createSprite(400, 200, 50, 50);
+
+  stand1 = new Ground(390,height - 100,200,20);
+  stand2 = new Ground(650,height - 200,200,20);
+  ground1 = new Ground(400,height + 50,800,20);
+
+  box1 = new Box(330, 235, 30, 40);
+  box2 = new Box(360, 235, 30, 40);
+  box3 = new Box(390, 235, 30, 40);
+  box4 = new Box(420, 235, 30, 40);
+  box5 = new Box(450, 235, 30, 40);
+
+  box6 = new Box(360, 195, 30, 40);
+  box7 = new Box(390, 195, 30, 40);
+  box8 = new Box(420, 195, 30, 40);
+
+  box9 = new Box(390, 185, 30, 40);
+
+  box10 = new Box(590, 135, 30, 40);
+  
+
+  polygon1 = new Polygon(100, 280, 40);
+
+  slingShot = new SlingShot(polygon1.body, {x: 150, y: 150});
+}
+
+function draw() {
+  if(backgroundImg) {
+    background(backgroundImg);
+}
+  
+  
+
+  Engine.update(engine);
+
+  
+  box1.score();
+ box1.display();
+  box2.score();
+  box2.display();
+  box3.score();
+  box3.display();
+  box4.score();
+  box4.display();
+  box5.score();
+  box5.display();
+  box6.score();
+ box6.display();
+  box7.score();
+  box7.display();
+  box8.score();
+  box8.display();
+  box9.score();
+  box9.display();
+  box10.score();
+ 
+  stand1.display();
+  
+  ground1.display();
+  polygon1.display();
+  slingShot.display();
+}
+
+function mouseDragged(){
+  if(gameState!=="launched"){
+    Matter.Body.setPosition(polygon1.body, {x: mouseX , y: mouseY});
+  }
+}
+
+function mouseReleased() {
+  slingShot.fly();
+  gameState = "launched";
+}
+
+function keyPressed() {
+  if(keyCode === 32) {
+      slingShot.attach(polygon1.body);
+      gameState = "on slingshot";
+  }
+}
+
+async function getBackgroundImg() {
+  var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+  var date = responseJSON.datetime;
+  var hour = date.slice(11, 13);
+  if(hour>=06 && hour<=19){
+      bg = "Sprites/bg.jpg"
+  } else {
+      bg = "Sprites/bg1.jpg"
+  }
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
+}
